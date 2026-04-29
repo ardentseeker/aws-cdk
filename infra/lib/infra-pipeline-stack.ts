3710a0dfa564177b64510572ef07fe120782df45
Roushangopal <roushanraja0@gmail.com>
2026-04-29 15:35:42 +0530

    initial infra

--- infra/lib/infra-pipeline-stack.ts
+++ infra/lib/infra-pipeline-stack.ts
@@ -1,30 +1,28 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as pipelines from 'aws-cdk-lib/pipelines';
-import { InfraStack } from './infra-stack';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as pipelines from "aws-cdk-lib/pipelines";
+import { InfraStack } from "./infra-stack";
 
 export class InfraPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const source = pipelines.CodePipelineSource.gitHub('roush/infra', 'main', {
-      authentication: cdk.SecretValue.secretsManager('github-token'),
+    const source = pipelines.CodePipelineSource.gitHub("roush/infra", "main", {
+      authentication: cdk.SecretValue.secretsManager("github-token"),
     });
-    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-      pipelineName: 'InfraPipeline',
-      synth: new pipelines.ShellStep('Synth', { 
+    const pipeline = new pipelines.CodePipeline(this, "Pipeline", {
+      pipelineName: "InfraPipeline",
+      synth: new pipelines.ShellStep("Synth", {
         input: source,
-        commands: [
-          'npm ci',
-          'cdk synth'
-        ]
-      })
+        commands: ["cd infra", "npm ci", "cdk synth"],
+      }),
     });
-    pipeline.addStage(new DeployInfraStackStage(this, 'DeployInfraStack'));}
+    pipeline.addStage(new DeployInfraStackStage(this, "DeployInfraStack"));
+  }
 }
 
 export class DeployInfraStackStage extends cdk.Stage {
   constructor(scope: Construct, id: string, props?: cdk.StageProps) {
     super(scope, id, props);
-    new InfraStack(this, 'InfraStack');
+    new InfraStack(this, "InfraStack");
   }
 }
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,8 +1,8 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-import * as ecs from 'aws-cdk-lib/aws-ecs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
@@ -11,68 +11,71 @@ export class InfraStack extends cdk.Stack {
     const vpc = this.createVPC(this);
     const dockerImage = this.createAssets(this);
     this.createEcsWebService(this, vpc, dockerImage);
-
   }
 
   createAssets(scope: Construct) {
-    const dockerImage = ecs.ContainerImage.fromAsset('../');
+    const dockerImage = ecs.ContainerImage.fromAsset("../");
     return dockerImage;
   }
 
   createVPC(scope: Construct): ec2.Vpc {
     // Create a VPC with public and private subnets
-    const vpc = new ec2.Vpc(scope, 'MyVPC', {
-      vpcName: 'MyVPC',
+    const vpc = new ec2.Vpc(scope, "MyVPC", {
+      vpcName: "MyVPC",
       maxAzs: 2,
       natGateways: 1,
     });
     return vpc;
   }
 
-  createEcsWebService(scope: Construct, vpc: ec2.Vpc, dockerImage: ecs.ContainerImage) {
+  createEcsWebService(
+    scope: Construct,
+    vpc: ec2.Vpc,
+    dockerImage: ecs.ContainerImage,
+  ) {
     // Create an ECS cluster
-    const cluster = new ecs.Cluster(scope, 'EcsCluster', {
+    const cluster = new ecs.Cluster(scope, "EcsCluster", {
       vpc: vpc,
-      clusterName: 'MyEcsCluster',
+      clusterName: "MyEcsCluster",
     });
 
     // Create a Fargate task definition
-    const taskDefinition = new ecs.FargateTaskDefinition(scope, 'TaskDef', {
+    const taskDefinition = new ecs.FargateTaskDefinition(scope, "TaskDef", {
       memoryLimitMiB: 512,
       cpu: 256,
     });
 
     // Add a container to the task definition
-    const container = taskDefinition.addContainer('WebContainer', {
+    const container = taskDefinition.addContainer("WebContainer", {
       image: dockerImage,
-      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'WebContainer' }),
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: "WebContainer" }),
     });
 
     // Create a Fargate service
-    const service = new ecs.FargateService(scope, 'FargateService', {
+    const service = new ecs.FargateService(scope, "FargateService", {
       cluster,
       taskDefinition,
       desiredCount: 2,
     });
 
     // Output the service URL
-    new cdk.CfnOutput(scope, 'ServiceURL', {
+    new cdk.CfnOutput(scope, "ServiceURL", {
       value: `http://${service.loadBalancer.loadBalancerDnsName}`,
     });
 
     //Create an Application Load Balancer
-    const loadBalancer = new elbv2.ApplicationLoadBalancer(scope, 'ALB', {
+    const loadBalancer = new elbv2.ApplicationLoadBalancer(scope, "ALB", {
       vpc,
       internetFacing: true,
     });
 
     // Add a listener to the load balancer
-    const listener = loadBalancer.addListener('Listener', {
+    const listener = loadBalancer.addListener("Listener", {
       port: 80,
     });
 
     // Add the ECS service as a target to the listener
-    listener.addTargets('EcsTargets', {
+    listener.addTargets("EcsTargets", {
       port: 80,
       targets: [service],
     });
