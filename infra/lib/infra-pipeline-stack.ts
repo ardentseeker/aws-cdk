1a6a19d397a7b29e1e1e45a35583b3c1f1a1e72d
Roushangopal <roushanraja0@gmail.com>
2026-04-29 16:41:43 +0530

    using code commit

--- infra/lib/infra-pipeline-stack.ts
+++ infra/lib/infra-pipeline-stack.ts
@@ -2,18 +2,18 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as pipelines from "aws-cdk-lib/pipelines";
 import { InfraStack } from "./infra-stack";
+import * as codecommit from "aws-cdk-lib/aws-codecommit";
 
 export class InfraPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const source = pipelines.CodePipelineSource.connection(
-      "ardentseeker/aws-cdk",
-      "main",
-      {
-        connectionArn:
-          "arn:aws:codeconnections:us-east-1:639493421189:connection/8cc9db2e-8d57-4dfb-b33d-32ec7137d645",
-      },
+
+    const repo = codecommit.Repository.fromRepositoryName(
+      this,
+      "CodeRepo",
+      "aws-cdk",
     );
+    const source = pipelines.CodePipelineSource.codeCommit(repo, "main");
     const pipeline = new pipelines.CodePipeline(this, "Pipeline", {
       pipelineName: "InfraPipeline",
       selfMutation: true,
